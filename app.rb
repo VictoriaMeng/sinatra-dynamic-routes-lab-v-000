69e70a7ce1dafd217d00699ae379abf72f9e315a
Victoria Meng <nights1ststar@protonmail.com>
2018-05-22 21:04:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation].to_s
+    @operation = params[:operation].to_sym
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
     "#{@number1.send(@operation, @number2)}"
